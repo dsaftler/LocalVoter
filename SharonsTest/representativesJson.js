bb1dbd5e3fc94f5d52e5787ff2aedec9691eda98
sdw03c <sharn.dwong@gmail.com>
2019-06-26 15:43:30 -0400

    Bills and Reps API info, variables, and routers

---
+++ SharonsTest/representativesJson.js
@@ -0,0 +1,26 @@
+var axios = require("axios")
+    var zipcode = "33602"
+    var googlAPI = "AIzaSyCbC-q0n1vMIzkpr7k7HvngMZTD9OxrnlQ"
+    var officials = []
+
+
+
+
+    axios.get("https://www.googleapis.com/civicinfo/v2/representatives?address="+zipcode+"&key="+googlAPI).then(function(res){
+        console.log(res.data)    
+    for(i=2; i <= 5; i++){
+        var repName = res.data.officials[i].name;
+        var party = res.data.officials[i].party;
+        var phones = res.data.officials[i].phones;
+        var urls = res.data.officials[i].urls;
+        var photoUrl = res.data.officials[i].photoUrl;
+        var socialMedia = res.data.officials[i].channels;
+
+
+            console.log(res.data.officials[i])
+            var test2 = res.data.officials[i]
+        //    officials.push(test2) 
+           officials.push({repName, party, phones, urls, photoUrl,socialMedia}) 
+        }
+    })
+        module.exports = { officials: officials}
