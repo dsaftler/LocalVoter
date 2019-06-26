2ba72f7601ecbd4c7e9a13f163089c96ffff02e1
Paul Davis <pauldavis@Pauls-MacBook-Pro.local>
2019-06-25 21:05:20 -0400

    adding html routes for home, login, signup

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,7 +1,15 @@
 module.exports = function(app) {
   app.get("/", function(req, res) {
-    res.render("login");
+    res.render("home")
   });
+
+  app.get("signup", function(req, res) {
+    res.render("signup");
+  });
+
+  app.get("login", function(req, res) {
+    res.render("login");
+  })
 
   app.get("/bills", function(req, res) {
     res.render("bill-display");
